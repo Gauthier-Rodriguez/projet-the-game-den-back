4c9a627667df7539f859fa4eea6ebd182998fd5e
Gauthier-Rodriguez <gauthier.rodriguez@oclock.school>
2024-01-11 17:22:31 +0100

    add get + patch on user info

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -14,36 +14,36 @@ const isAuthenticated = async (req, res) => {
 
 const createUser = async (req, res) => {
   const {firstname, lastname, pseudo, email, password} = req.body; 
-  
+    
   if (!firstname || !lastname || !pseudo) {
-        return res.status(400).json({ error: 'All fields are required' });
-    }
-
+      return res.status(400).json({ error: 'All fields are required' });
+  }
+  
   const pseudoExist = await User.findOne({where: {Pseudo : req.body.pseudo}});
+  
   if(pseudoExist) return res.status(400).send('Pseudo already taken');
-
+  
   const emailExist = await User.findOne({where: {Email : req.body.email}});
+  
   if(emailExist) return res.status(400).send('Email already taken');
-
+  
   if (!validator.isEmail(email)) {
-        return res.status(400).json({ error: 'Invalid email address'});
-    }
-
-     if (!validator.isStrongPassword(password)) {
-        return res.status(400).json({ error: 'Weak password. Please choose a stronger password'});
-    }
-
+      return res.status(400).json({ error: 'Invalid email address'});
+  };
+  
+  if (!validator.isStrongPassword(password)) {
+      return res.status(400).json({ error: 'Weak password. Please choose a stronger password'});
+  };
+  
   const salt = await bcrypt.genSalt(10);
   const hashPassword = await bcrypt.hash(req.body.password, salt);
-
   
-
   if(typeof firstname !== "string")
-  {res.status(400).json({ error : 'Your first name is not valid'})};
-
+    {res.status(400).json({ error : 'Your first name is not valid'})};
+  
   if(typeof lastname !== "string")
-  {res.status(400).json({ error : 'Your last name is not valid'})};
-
+    {res.status(400).json({ error : 'Your last name is not valid'})};
+  
   const user = new User({
     FirstName : req.body.firstname,
     LastName : req.body.lastname,
@@ -66,10 +66,83 @@ const loginUser = async (req, res) => {
   const token = jwt.sign({pseudo : user.pseudo, email : user.email}, process.env.SECRET);
   res.header('auth-token', token);
   res.json(token);
-}
+};
+
+const getAllInfos = async (req, res) => {
+  const userId = parseInt(req.params.id);
+
+  if (isNaN(userId)){
+    return res.status(400).json({error : "Invalid userId provided"})
+  }
+  const infos = await User.findByPk(userId);
+  
+  if(!infos) {
+    return res
+    .status(404)
+    .json({error : 'User not found'})
+    
+  };
+
+  res.status(200).json(infos)
+};
+
+const updateInfos = async (req, res) => {
+    const userId = parseInt(req.params.id);
+
+    if(isNaN(userId)) {
+        return res.status(400).json({ error: "Invalid userId provided." });
+    }
+
+    const { firstname, lastname, pseudo, email, password } = req.body; 
+    
+    if (firstname && typeof firstname !== 'string') {
+      return res.status(400).json({ error: "Invalid type for property 'firstname'" });
+    }
+
+    if (lastname && typeof lastname !== 'string') {
+      return res.status(400).json({ error: "Invalid type for property 'lastname'" });
+    }
+
+    
+    if (pseudo && typeof pseudo !== 'string') {
+      return res.status(400).json({ error: "Invalid type for property 'pseudo'" });
+    }
+    if(req.body.Pseudo){
+    const pseudoExist = await User.findOne({where: {Pseudo : req.body.Pseudo}});
+    if(pseudoExist) return res.status(400).send('Pseudo already taken');
+    }
+
+    if (email && typeof email !== 'string') {
+      return res.status(400).json({ error: "Invalid type for property 'email'" });
+    }
+    
+    if(req.body.Email){
+    const emailExist = await User.findOne({where: {Email : req.body.Email}});
+    if(emailExist) return res.status(400).send('Email already taken');
+  }
+
+
+    const user = await User.findByPk(userId);
+    
+    if (!user) {
+      return res.status(404).json({ error : "User not found"})
+    };
+    
+    const updatedUser = await user.update({
+      Pseudo: req.body.Pseudo || user.Pseudo,
+      LastName: req.body.LastName|| user.LastName,
+      FirstName: req.body.FirstName || user.FirstName,
+      Email: req.body.Email || user.Email,
+      Password: req.body.Password || user.Password
+    });
+    
+    res.status(200).json(updatedUser);
+};
 
 module.exports = {
   isAuthenticated,
   createUser,
-  loginUser
+  loginUser,
+  getAllInfos,
+  updateInfos
 };
