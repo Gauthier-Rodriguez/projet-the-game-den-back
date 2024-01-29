b7f8e9b8d805e69976baf0726f147475d9a3c80a
Gauthier-Rodriguez <gauthier.rodriguez@oclock.school>
2024-01-29 16:09:14 +0100

    Add rating and rating count filters to game recommendation API query and update PC platform logo

--- src/utils/getLogo.js
+++ src/utils/getLogo.js
@@ -5,7 +5,7 @@ const getLogo = (platform) => {
     if(platformName.includes('playstation')) return 'playstation.svg';
     if(platformName.includes('xbox')) return 'xbox.svg';
     if(nintendoPlatforms.some((p) => platformName.includes(p))) return 'nintendo.svg';
-    if(platformName.includes('pc')) return 'pc.svg';
+    if(platformName.includes('windows')) return 'pc.svg';
     if(platformName.includes('ios')) return 'ios.svg';
     if(platformName.includes('android')) return 'android.svg';
     if(platformName.includes('mac')) return 'mac.svg';
